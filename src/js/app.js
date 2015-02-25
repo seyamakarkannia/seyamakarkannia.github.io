ba850a5d05b3d4182fda00d1f9da76366821445f
Sia <Seyamak@Seyamaks-Air-2.lan>
2015-02-25 23:13:00 +0100

    Added logo and social links for temporary design

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,3 @@
 //
 // APP
 //
-
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('ready');
-});
-
-var openNav = $('.open-nav');
-var blocker = $('.blocker');
-
-openNav.click(function() {
-  $('body').addClass('nav-open');
-});
-
-blocker.click(function() {
-  $('body').removeClass('nav-open');
-})
